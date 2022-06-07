1c1fd5700f094214fe8e6f0d030e54897ab51b99
Isaac <you@example.com>
2022-06-06 19:01:17 -0500

    stuff

--- main.py
+++ main.py
@@ -7,18 +7,26 @@ file.close()
 code = ""
 
 for line in range(len(lines)):
+    lines[line] = lines[line].strip()
     for char in range(len(lines[line])):
-        if lines[line][char] == "/":
-            if lines[line][char + 1] == "/":
-                del lines[line][char:-1]
-        elif lines[line][char] == "@":
-            if lines[line][char + 1] == "O":
-                if lines[line][char + 2] == "v":
-                    if lines[line][char + 3] == "e":
-                        if lines[line][char + 4] == "r":
-                            if lines[line][char + 5] == "r":
-                                if lines[line][char + 6] == "i":
-                                    if lines[line][char + 7] == "d":
-                                        if lines[line][char + 8] == "e":
-                                            lines[line].append(" ")
-        code = code + lines[line]
+        try:
+            if lines[line][char] == "/":
+                if lines[line][char + 1] == "/":
+                    lines[line] = lines[line][0:char]
+            elif lines[line][char] == "@":
+                if lines[line][char + 1] == "O":
+                    if lines[line][char + 2] == "v":
+                        if lines[line][char + 3] == "e":
+                            if lines[line][char + 4] == "r":
+                                if lines[line][char + 5] == "r":
+                                    if lines[line][char + 6] == "i":
+                                        if lines[line][char + 7] == "d":
+                                            if lines[line][char + 8] == "e":
+                                                lines[line] += " "
+        except IndexError:
+            pass
+    code += lines[line]
+
+file = open(file_name, "w")
+file.write(code)
+file.close()
