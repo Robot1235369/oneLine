cefda631852bb54933c6b2dbdd4d641352825075
Isaac <you@example.com>
2022-06-06 19:04:02 -0500

    stuff

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
-file_name = input("file path: ")
+file_names = ["enter files here"]
 
-file = open(file_name, "r")
-lines = file.readlines()
-file.close()
+for i in range(len(file_names)):
+    file = open(file_names[i], "r")
+    lines = file.readlines()
+    file.close()
 
-code = ""
+    code = ""
 
-for line in range(len(lines)):
-    lines[line] = lines[line].strip()
-    for char in range(len(lines[line])):
-        try:
-            if lines[line][char] == "/":
-                if lines[line][char + 1] == "/":
-                    lines[line] = lines[line][0:char]
-            elif lines[line][char] == "@":
-                if lines[line][char + 1] == "O":
-                    if lines[line][char + 2] == "v":
-                        if lines[line][char + 3] == "e":
-                            if lines[line][char + 4] == "r":
-                                if lines[line][char + 5] == "r":
-                                    if lines[line][char + 6] == "i":
-                                        if lines[line][char + 7] == "d":
-                                            if lines[line][char + 8] == "e":
-                                                lines[line] += " "
-        except IndexError:
-            pass
-    code += lines[line]
+    for line in range(len(lines)):
+        lines[line] = lines[line].strip()
+        for char in range(len(lines[line])):
+            try:
+                if lines[line][char] == "/":
+                    if lines[line][char + 1] == "/":
+                        lines[line] = lines[line][0:char]
+                elif lines[line][char] == "@":
+                    if lines[line][char + 1] == "O":
+                        if lines[line][char + 2] == "v":
+                            if lines[line][char + 3] == "e":
+                                if lines[line][char + 4] == "r":
+                                    if lines[line][char + 5] == "r":
+                                        if lines[line][char + 6] == "i":
+                                            if lines[line][char + 7] == "d":
+                                                if lines[line][char + 8] == "e":
+                                                    lines[line] += " "
+            except IndexError:
+                pass
+        code += lines[line]
 
-file = open(file_name, "w")
-file.write(code)
-file.close()
+    file = open(file_names[i], "w")
+    file.write(code)
+    file.close()
